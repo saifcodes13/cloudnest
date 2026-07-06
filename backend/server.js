fb5b0b0eca29eaa417ce33f60a69180bc6002d21
saifcodes13 <saifcodes13@gmail.com>
2026-07-06 18:48:27 +0000

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res) => {
+res.send("Saif Cloud Backend is Running")
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
+
